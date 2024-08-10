3ddbde1a96be995910910628d978b7860fcc9c80
USER <USER@ADMIN>
2024-08-10 17:26:52 +0700

    ver 1

---
+++ content/page-script.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const message = "Anh rất thích em , cái cách em quan tâm đến anh và chuyện tình cảm của anh và em. Đó là điều khiến em không chỉ dễ thương mà còn thật đặc biệt trong mắt anh.";
+    const typingSpeed = 50; // Speed in milliseconds
+
+    const messageElement = document.getElementById('message-text');
+    let index = 0;
+
+    function typeMessage() {
+        if (index < message.length) {
+            messageElement.textContent += message[index++];
+            setTimeout(typeMessage, typingSpeed);
+        }
+    }
+
+    typeMessage();
+
+});
--- script.js
+++ script.js
@@ -2,30 +2,24 @@
 let interval = setInterval(() => {
     if (progress >= 100) {
         clearInterval(interval);
-        showFinalScreen(); // Gọi hàm thay thế hình nền và phát nhạc khi hoàn thành
+        showCuteButton(); // Hiển thị nút cute sau khi hoàn thành progress bar
     } else {
         progress += 2; // Tăng mức độ lên 2%
         document.getElementById('progress').style.width = progress + '%';
     }
 }, 50); // Giảm thời gian chờ xuống 50ms
 
-function showFinalScreen() {
-    // Thay thế hình nền của trang
-    document.body.style.backgroundImage = "url('https://picsum.photos/200')";
-
+function showCuteButton() {
     // Ẩn thanh progress bar và loading text
     document.getElementById('loading-container').style.display = 'none';
     document.getElementById('loading-text').style.display = 'none';
 
-    // Hiển thị đĩa than và phát nhạc
-    let recordContainer = document.getElementById('record-container');
-    recordContainer.style.display = 'flex'; // Hiển thị đĩa than
+    // Hiển thị nút "cute"
+    let cuteButton = document.getElementById('cute-button');
+    cuteButton.style.display = 'block';
+}
 
-    let audio = document.getElementById('audio');
-    audio.play(); // Phát nhạc tự động khi hiển thị đĩa than
-
-    // Dừng đĩa than khi nhạc kết thúc
-    audio.onended = function() {
-        document.getElementById('record').style.animationPlayState = 'paused';
-    };
+// Hàm chuyển trang khi nhấn nút
+function goToPage() {
+    window.location.href = "page.html"; 
 }
